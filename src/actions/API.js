a120347c7cbff335366e3874b8d3d1b1f911330a
justinlew <lew.justin95@gmail.com>
2019-07-22 23:51:13 -0700

    meh

--- src/actions/API.js
+++ src/actions/API.js
@@ -1,15 +1,12 @@
 import axios from 'axios'
+import history from '../history'
 
 const development = process.env.NODE_ENV !== 'production'
 
 console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 console.log("Is development ", development)
 
-const API = axios.create({
-    baseURL: development ? "http://localhost:3000" : "https://mighty-waters-11379.herokuapp.com"
-})
-
-API.interceptors.request.use(
+axios.interceptors.request.use(
     config => {
         const token = localStorage.getItem('token')
         console.log(config)
@@ -21,5 +18,22 @@ API.interceptors.request.use(
     error => {
         return Promise.reject(error)
 })
+
+axios.interceptors.response.use((response) =>  {
+	return response
+}, (error) => {
+	console.log("Error with axios interceptor", error)
+	if (error && error.response && error.response.status === 401) {
+		return Promise.reject(
+			history.push('/login')
+		);
+	}
+})
+
+const API = axios.create({
+    baseURL: development ? "http://localhost:3000" : "https://mighty-waters-11379.herokuapp.com"
+})
+
+
 
 export default API
