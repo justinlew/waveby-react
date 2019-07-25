495d13c7ad842074aaa587cbfa1e3e222df273a7
justinlew <lew.justin95@gmail.com>
2019-07-24 19:58:55 -0700

    Another test

--- src/actions/API.js
+++ src/actions/API.js
@@ -12,29 +12,29 @@ const API = axios.create({
     baseURL: development ? "http://localhost:3000" : "https://mighty-waters-11379.herokuapp.com"
 })
 
-API.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('token')
-        console.log(config)
-        if (token) {
-            config.headers.authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    error => {
-        return Promise.reject(error)
-})
+// API.interceptors.request.use(
+//     config => {
+//         const token = localStorage.getItem('token')
+//         console.log(config)
+//         if (token) {
+//             config.headers.authorization = `Bearer ${token}`
+//         }
+//         return config
+//     },
+//     error => {
+//         return Promise.reject(error)
+// })
 
-API.interceptors.response.use((response) =>  {
-	return response
-}, (error) => {
-	console.log("Error with axios interceptor", error)
-	if (error && error.response && error.response.status === 401) {
-		return Promise.reject(
-			history.push('/login')
-		);
-	}
-})
+// API.interceptors.response.use((response) =>  {
+// 	return response
+// }, (error) => {
+// 	console.log("Error with axios interceptor", error)
+// 	if (error && error.response && error.response.status === 401) {
+// 		return Promise.reject(
+// 			history.push('/login')
+// 		);
+// 	}
+// })
 
 
 
